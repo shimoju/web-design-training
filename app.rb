90fb16ac0a249e743b6775d3e0a5e3d1d8c92595
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2014-08-22 19:49:38 +0900

    Sinatra::Reloaderを有効化

--- app.rb
+++ app.rb
@@ -1,6 +1,11 @@
 require 'sinatra/base'
+require 'sinatra/reloader'
 
 class App < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
+
   get '/' do
     erb :index
   end
