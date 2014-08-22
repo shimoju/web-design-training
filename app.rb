f525d3463b3dcee3d2226c6f8d2102adcfd2a3f6
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2014-08-22 20:17:26 +0900

    セッションを有効化

--- app.rb
+++ app.rb
@@ -2,6 +2,10 @@ require 'sinatra/base'
 require 'sinatra/reloader'
 
 class App < Sinatra::Base
+  configure do
+    enable :sessions
+  end
+
   configure :development do
     register Sinatra::Reloader
   end
